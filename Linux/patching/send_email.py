cb00edaea4297418ffd68d8d79e9942dda54a9a5
Brian Hartley <brian@gridnorth.tech>
2024-08-02 15:08:12 -0600

    Folder rename, added Kubernetes node setup playbook

---
+++ Linux/patching/send_email.py
@@ -0,0 +1,64 @@
+# Core imports
+import os
+import sys
+
+# 3rd-party imports
+import sendgrid  # pip install sendgrid
+from sendgrid.helpers.mail import Mail, Email, To, Content
+from dotenv import load_dotenv  # pip install python-dotenv
+
+# Local app imports
+# none at this time
+
+
+# Notes
+# From: https://docs.sendgrid.com/for-developers/sending-email/quickstart-python
+# Response codes: https://docs.sendgrid.com/api-reference/how-to-use-the-sendgrid-v3-api/responses#status-codes
+
+
+# Get the hostname
+name = os.popen('hostname').read().split("\n")[0]
+summary_msg = "Patching completed on {}".format(name)
+
+
+# Get the file path from the passed arg
+# Note this is a bad plan lol. Do some input validation here.
+# This sets the email body (the 'content' var) to the text in the dnf history info file
+if len(sys.argv) > 1:
+    file_path = str(sys.argv[1])
+    file = open(file_path, 'r')
+    file_contents = file.read()
+    content = Content("text/plain", file_contents)
+    file.close()
+else:
+    # No file path arg (or any arg) was passed
+    content = Content("text/plain", summary_msg)
+
+
+
+# Load the .env file
+load_dotenv()
+
+
+# Initial variables
+sg_api_key = os.getenv('SENDGRID_API_KEY')
+
+
+# Build the mail object parameters
+sg = sendgrid.SendGridAPIClient(api_key=sg_api_key)
+from_email = Email("alerts@mydomain.com")
+to_email = To("me@mydomain.com")
+subject = "ALERT: {}".format(summary_msg)
+mail = Mail(from_email, to_email, subject, content)
+
+
+# get a JSON-ready representation of the mail object
+mail_json = mail.get()
+
+
+# Send an HTTP POST request to /mail/send
+response = sg.client.mail.send.post(request_body=mail_json)
+print(response.status_code)  # Code 202 = success (see response code link above)
+print(response.headers)
+
+
